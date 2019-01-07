f69581058c9c8b833d04e0a0287ec1fe2bc7865d
rene <renegmed@yahoo.com>
2019-01-07 12:58:46 -0500

    2-basics: positioning elements with flexbox

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, TextInput} from 'react-native';
+import {Platform, StyleSheet, Text, View, TextInput, Button} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -31,11 +31,16 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}> 
-        <TextInput 
-          style={{ width: 300}}
-          placeholder="An awesome place"
-          value={this.state.placeName}
-          onChangeText={this.placeNameChangedHandler}></TextInput>
+        <View style={styles.inputContainer}>
+          <TextInput 
+            style={styles.placeInput}
+            placeholder="An awesome place"
+            value={this.state.placeName}
+            onChangeText={this.placeNameChangedHandler}
+          /> 
+          <Button title="Add" style={styles.placeButton}/>  
+        </View>
+       
       </View>
     );
   }
@@ -48,5 +53,18 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-start',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
+  },
+  inputContainer: {
+    //flex: 1,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: 'center' 
+  },
+  placeInput: {
+    width: "70%"
+  },
+  placeButton: {
+    width: "30%"
   } 
 });
