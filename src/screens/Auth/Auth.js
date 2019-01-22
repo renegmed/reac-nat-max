8a41f8474cffbb4af3ee4e7460edad629a0d2675
rene <renegmed@yahoo.com>
2019-01-22 17:35:02 -0500

    6-style: styling text

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -4,7 +4,7 @@ import {  View, Text, TextInput, Button, StyleSheet } from 'react-native';
 import { pushFindPlace } from '../../navigation';
 
 import DefaultInput from "../../components/UI/DefaultInput";
-
+import HeadingText from "../../components/UI/HeadingText";
 class Auth extends Component {
  
   loginHandler = () => {
@@ -14,11 +14,11 @@ class Auth extends Component {
   render() {
     return (
       <View style={styles.container}> 
-            <Text>Please Log In</Text> 
+            <HeadingText>Please Log In</HeadingText> 
             <Button title="Switch to Login" onPress={this.loginHandler} />
             <View style={styles.inputContainer}> 
               <DefaultInput placeholder="Your E-Mail Address" style={styles.input}/>
-              <DefaultInput placeholder="Password" style={[styles.input, { borderColor: "red" } ]}/>
+              <DefaultInput placeholder="Password" style={styles.input}/>
               <DefaultInput placeholder="Confirm" style={styles.input}/>
             </View>
             
@@ -36,7 +36,7 @@ const styles = StyleSheet.create({
   },
   inputContainer: {
     width: "60%"
-  },
+  }, 
   input: {
     backgroundColor: "#eee",
     borderColor: "#bbb"
