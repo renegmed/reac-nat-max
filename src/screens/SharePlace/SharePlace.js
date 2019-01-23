ea7257b72819aec48e36ce43e75108904764df98
rene <renegmed@yahoo.com>
2019-01-22 19:21:25 -0500

    6-style: editing the 'share place' screen, add image preview and map boxes

--- src/screens/SharePlace/SharePlace.js
+++ src/screens/SharePlace/SharePlace.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, Text, Image, Button, StyleSheet,ScrollView} from 'react-native';
 import { connect } from 'react-redux';
 import { Navigation } from 'react-native-navigation';
 
-import PlaceInput from '../../components/PlaceInput/PlaceInput';
+ 
 import { addPlace } from '../../store/actions/index';  
+import DefaultInput from '../../components/UI/DefaultInput';
+import MainText from '../../components/UI/MainText';
+import HeadingText from '../../components/UI/HeadingText';
+import imagePlaceholder from '../../assets/beautiful-place.jpg';
 
 class SharePlace extends Component { 
  
@@ -41,13 +45,59 @@ class SharePlace extends Component {
     
     render() {
         return (
-            <View>
-                <PlaceInput onPlaceAdded={this.placeAddedHandler}/>  
-            </View>
+            <ScrollView >
+                <View style={styles.container}>
+                    <MainText>
+                        <HeadingText>Share a Place with us!</HeadingText>
+                    </MainText>
+                     
+                    <View style={styles.placeholder}>
+                       <Image source={imagePlaceholder} style={styles.previewImage} />
+                    </View> 
+                    <View style={styles.button}>
+                        <Button title="Pick Image" />
+                    </View>
+                    
+                    <View style={styles.placeholder}>
+                        <Text>Map</Text>
+                    </View> 
+                    <View style={styles.button}>
+                        <Button title="Locate Me" />
+                    </View>
+                    
+                    <DefaultInput placeholder="Place Name" />
+
+                    <View style={styles.button}>
+                        <Button title="Share the Place!" /> 
+                    </View>
+                    
+                </View>  
+                
+            </ScrollView>
         )
     }
 }
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center"
+    },
+    placeholder: {
+        borderWidth: 1,
+        borderColor: "black",
+        backgroundColor: "#eee",
+        width: "80%",
+        height: 150
+    },
+    button: {
+        margin: 8,
 
+    },
+    previewImage: {
+        width: "100%",
+        height: "100%"
+    }
+})
 const mapDispatchToProps = dispatch => {
     return {
         onAddPlace: (placeName) => dispatch(addPlace(placeName))
