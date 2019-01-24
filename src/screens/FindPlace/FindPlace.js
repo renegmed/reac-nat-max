a36badf94143fe6e90548034c68148779846fb1f
rene <renegmed@yahoo.com>
2019-01-24 13:13:17 -0500

    6-style: using the Animated API

--- src/screens/FindPlace/FindPlace.js
+++ src/screens/FindPlace/FindPlace.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, Animated } from 'react-native';
 import { Navigation} from 'react-native-navigation' 
 import { connect } from 'react-redux'; 
 
@@ -9,7 +9,8 @@ import constant from '../../constants';
 class FindPlace extends Component {   
     
     state = {
-        placesLoaded: false
+        placesLoaded: false,
+        removeAnim: new Animated.Value(1)
     }
 
     componentDidMount() {
@@ -62,17 +63,33 @@ class FindPlace extends Component {
     }
 
     placesSearchHandler = () => {
-        this.setState({
-            placesLoaded: true
-        });
+       Animated.timing(this.state.removeAnim, {
+            toValue: 0,
+            duration: 500,
+            userNativeDriver: true
+        }).start();
     }
     render() { 
         let content = (
-            <TouchableOpacity onPress={ this.placesSearchHandler }>
-                <View style={styles.searchButton} >
-                    <Text style={styles.searchButtonText}>Find Place</Text>
-                </View>
-            </TouchableOpacity>
+            <Animated.View
+                style={{
+                    opacity: this.state.removeAnim,
+                    transform: [
+                        {
+                            scale: this.state.removeAnim.interpolate({
+                                inputRange: [0, 1],
+                                outputRange: [12, 1]
+                            })
+                        }
+                    ]
+                }}    >
+                <TouchableOpacity onPress={ this.placesSearchHandler }>
+                    <View style={styles.searchButton} >
+                        <Text style={styles.searchButtonText}>Find Place</Text>
+                    </View>
+                </TouchableOpacity>
+            </Animated.View>
+           
         )
         
         if (this.state.placesLoaded) {
