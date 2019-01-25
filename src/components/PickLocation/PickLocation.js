2cc705b5ef0be50e8004888bfd8df6e1251a4097
rene <renegmed@yahoo.com>
2019-01-25 12:31:22 -0500

    8-device-features: picking a location on the map

--- src/components/PickLocation/PickLocation.js
+++ src/components/PickLocation/PickLocation.js
@@ -12,12 +12,28 @@ class PickLocation extends Component {
                 (Dimensions.get("window").width / Dimensions.get("window").height) * 0.0122 
         }
     }
+
+    pickLocationHandler = event => {
+        const coords = event.nativeEvent.coordinate;
+        this.setState(prevState => {
+            return {
+                focusedLocation: {
+                    ...prevState.focusedLocation,
+                    latitude: coords.latitude,
+                    longitude: coords.longitude
+                }
+            }     
+        });
+    }
+
     render() {
         return (
             <View style={styles.container}>
                 <MapView 
                     initialRegion={this.state.focusedLocation}
+                    region={this.state.focusedLocation}
                     style={styles.map}
+                    onPress={this.pickLocationHandler}
                 /> 
                 <View style={styles.button}>
                     <Button 
