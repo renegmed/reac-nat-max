e24bd7ff00db7feaf518039df4de2ebc77678426
rene <renegmed@yahoo.com>
2019-01-26 15:08:29 -0500

    8-device-features: install and configure react-native-image-picker for android and ios

--- android/app/src/main/java/com/awesomeproject/MainActivity.java
+++ android/app/src/main/java/com/awesomeproject/MainActivity.java
@@ -1,15 +1,25 @@
 package com.awesomeproject;
 
 import com.reactnativenavigation.NavigationActivity;
+import com.imagepicker.permissions.OnImagePickerPermissionsCallback; 
+import com.facebook.react.modules.core.PermissionListener;  
 
-public class MainActivity extends NavigationActivity {
+public class MainActivity extends NavigationActivity implements OnImagePickerPermissionsCallback {
+    private PermissionListener listener;
+     
+    @Override
+    public void setPermissionListener(PermissionListener listener)
+    {
+      this.listener = listener;
+    }
 
-    /**
-     * Returns the name of the main component registered from JavaScript.
-     * This is used to schedule rendering of the component.
-     */
-    // @Override
-    // protected String getMainComponentName() {
-    //     return "AwesomeProject";
-    // }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults)
+    {
+      if (listener != null)
+      {
+        listener.onRequestPermissionsResult(requestCode, permissions, grantResults);
+      }
+      super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
 }
--- android/app/src/main/java/com/awesomeproject/MainApplication.java
+++ android/app/src/main/java/com/awesomeproject/MainApplication.java
@@ -3,6 +3,7 @@ package com.awesomeproject;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.imagepicker.ImagePickerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -13,6 +14,7 @@ import com.reactnativenavigation.react.NavigationReactNativeHost;
 import com.reactnativenavigation.react.ReactGateway;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.airbnb.android.react.maps.MapsPackage;
+import com.imagepicker.ImagePickerPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -39,7 +41,8 @@ public class MainApplication extends NavigationApplication {
       return Arrays.<ReactPackage>asList(
           // eg. new VectorIconsPackage()
           new VectorIconsPackage(),
-          new MapsPackage() 
+          new MapsPackage(), 
+          new ImagePickerPackage()
       );
   }
 
