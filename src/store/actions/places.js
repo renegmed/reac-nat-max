837a5ad2f23798d4d236fba9d0b0755379b8df6f
rene <renegmed@yahoo.com>
2019-01-26 20:34:17 -0500

    9-network: storing the remaining data (to database 'places')

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -3,10 +3,7 @@ import { ADD_PLACE, DELETE_PLACE} from './actionTypes';
 
 export const addPlace = (placeName, location, image) => { 
     return  dispatch =>{
-        const placeData = {
-            name: placeName,
-            location: location
-        }
+        
         fetch("https://us-central1-awesome-places-215c1.cloudfunctions.net/storeImage", {
             method: "POST",
             body: JSON.stringify({
@@ -16,9 +13,23 @@ export const addPlace = (placeName, location, image) => {
         .catch( err => console.log(err))  // VERY IMPORTANT: catch() will only catch failed network requests. It will NOT catch 4xx and 5xx error codes!
         .then(res => res.json())
         .then(parsedRes => {
+            const placeData = {
+                name: placeName,
+                location: location,
+                image: parsedRes.imageUrl
+            };
+            return fetch("https://awesome-places-215c1.firebaseio.com/places.json", {
+                method: "POST",
+                body: JSON.stringify(placeData)
+            })
+        })
+        .catch( err => console.log(err))
+        .then(res => res.json())
+        .then(parsedRes => {
             console.log(parsedRes);
         });
-            
+         
+
         // fetch("https://awesome-places-215c1.firebaseio.com/places.json",{
         //     method: "POST",
         //     body: JSON.stringify(placeData)
